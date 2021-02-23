0496725b29acfcfca9b14c2787393ff2f55e9668
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-02-23 16:11:29 -0500

    fix #12

--- controllers/ikigaiController.js
+++ controllers/ikigaiController.js
@@ -1,30 +1,30 @@
 const db = require("../models");
 
-// Defining methods for the recruiterController
+// Defining methods for the ikigaiController
 module.exports = {
   findAll: function (req, res) {
-    db.Recruitee.find(req.query)
+    db.Ikigai.find(req.query)
       .sort({ date: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   findById: function (req, res) {
-    db.Recruitee.findById(req.query.id)
+    db.Ikigai.findById(req.query.id)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   create: function (req, res) {
-    db.Recruitee.create(req.body)
+    db.Ikigai.create(req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   update: function (req, res) {
-    db.Recruitee.findByIdAndUpdate({ _id: req.params.id }, req.body)
+    db.Ikigai.findByIdAndUpdate({ _id: req.params.id }, req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   delete: function (req, res) {
-    db.Recruitee.findOneAndDelete({ _id: req.params.id }, req.body)
+    db.Ikigai.findOneAndDelete({ _id: req.params.id }, req.body)
       .then((dbModel) => dbModel.delete())
       .catch((err) => res.status(422).json(err));
   },
