dd93f89552d9597296a88061b153433713eb8341
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-02-22 20:27:38 -0500

    added wildcard route

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.get("/api/config", (req, res) => {
   });
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
