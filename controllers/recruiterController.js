0d841069dc31a6193f70d9d511dbe426699f4a6d
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-02-23 16:07:43 -0500

    fix #25

---
+++ controllers/recruiterController.js
@@ -0,0 +1,31 @@
+const db = require("../models");
+
+// Defining methods for the recruiteeController
+module.exports = {
+  findAll: function (req, res) {
+    db.Recruiter.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Recruiter.findById(req.query.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.Recruiter.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Recruiter.findByIdAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  delete: function (req, res) {
+    db.Recruiter.findOneAndDelete({ _id: req.params.id }, req.body)
+      .then((dbModel) => dbModel.delete())
+      .catch((err) => res.status(422).json(err));
+  },
+};
