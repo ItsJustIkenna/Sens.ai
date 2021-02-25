37b5597c025e13e7c7fb33b86d468b2e67b6c881
William Miller <williamhmiller12@gmail.com>
2021-02-25 09:53:50 -0500

    I added a basic title layout

---
+++ client/src/components/UserTitle/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UserTitle = ({ width, height, src, position, title }) => {
+  return (
+    <div className="column is-flex" style={{ justifyContent: position }}>
+      <section
+        className="button is-black"
+        style={{ backgroundColor: "black", height: "55px", width: "400px" , borderRadius : "100px" }}
+      >{title}</section>
+    </div>
+  );
+};
+
+export default UserTitle;
--- client/src/pages/user-profile.js
+++ client/src/pages/user-profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from '../components/Header_Signed_In/index'
 import UserMenu from '../components/UserMenu/index'
+import UserTitle from '../components/UserTitle/index'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function userProfile() {
@@ -8,6 +9,7 @@ function userProfile() {
   return (
     <div className="userProfile" style={{backgroundColor: "red", minHeight: '100vh'}}>
       <Header />
+      <UserTitle position="center" title="Jerome Parkins"/>
       <UserMenu position="right" />
       <div className="container is-widescreen" >
       
