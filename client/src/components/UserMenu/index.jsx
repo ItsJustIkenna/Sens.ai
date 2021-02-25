d0b7fbdb341382df263f70e3e5c04898db6626fc
William Miller <williamhmiller12@gmail.com>
2021-02-25 12:09:37 -0500

    I added the bio component

--- client/src/components/UserMenu/index.jsx
+++ client/src/components/UserMenu/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import UserPhoto from '../UserPhoto/index'
+import UserBio from '../UserBio/index'
 
 const UserMenu = ({ width, height, src, position }) => {
   return (
-    <div className="column is-flex" style={{ justifyContent: position }}>
+    <div className="column is-11 is-flex" style={{ justifyContent: position }}>
         <UserPhoto src={src} />
       <section
         className="container"
         style={{ backgroundColor: "black", height: "55vh", width: "100%" , borderRadius : "100px" }}
-      ></section>
+      ><UserBio Description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum neque non blanditiis dolore voluptate iure perferendis ut, nemo quidem, voluptas, voluptatibus ab nihil! Exercitationem, saepe maiores natus distinctio maxime odio." /></section>
     </div>
   );
 };
