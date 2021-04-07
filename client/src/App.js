fb1aa2d7e145f23182b52c66642ed2e0eb8c0444
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-04-07 18:52:05 -0400

    added account creation pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,35 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LandingPage from "./pages/landingPage";
 import HomePage from "./pages/homePage";
-import AccountCreationPage from "./pages/accountCreationPage";
 import LoginPage from "./pages/loginPage";
 import ProfilePage from "./pages/profilePage";
 import SignUpPage from "./pages/signupPage";
-import './App.css';
-
+import RecruitAccountCreationPage from "./pages/recruitAccountCreationPage";
+import RecruiterAccountCreationPage from "./pages/recruiterAccountCreationPage";
+import UserPage from "./pages/userPage";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Switch>
-          <Route exact path="/" component={LandingPage}/>
-          <Route exact path="/homepage/:id" component={HomePage}/>
-          <Route exact path="/signuppage" component={SignUpPage}/>
-          <Route exact path="/accountcreationpage" component={AccountCreationPage}/>
-          <Route exact path="/loginpage" component={LoginPage}/>
-          <Route exact path="/profilepage/:id" component={ProfilePage}/>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/homepage/:id" component={HomePage} />
+          <Route exact path="/signuppage" component={SignUpPage} />
+          <Route exact path="/userpage" component={UserPage} />
+          <Route
+            exact
+            path="/recruitaccountcreationpage"
+            component={RecruitAccountCreationPage}
+          />
+          <Route
+            exact
+            path="/recruiteraccountcreationpage"
+            component={RecruiterAccountCreationPage}
+          />
+          <Route exact path="/loginpage" component={LoginPage} />
+          <Route exact path="/profilepage/:id" component={ProfilePage} />
         </Switch>
       </Router>
     </div>
---
+++ client/src/components/recruitAccountCreationFormComponent/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import img from "../../img/black-men-in-the-workplace2.jpg";
+import logo from "../../img/higher-me-logo.png";
+import "./style.css";
+
+function index() {
+  return (
+    <body>
+      <section className="Form my-4 mx-5">
+        <div className="container">
+          <div className="row no-gutters">
+            <div className="col-lg-5">
+              <img src={img} className="img-fluid" alt="" />
+            </div>
+            <div className="col-lg-7 px-5 pt-5">
+              <img className="navbar__logo" src={logo} alt="" />
+              <form>
+                <div className="form-row">
+                  <div className="col-lg-7">
+                    <input
+                      type="name"
+                      placeholder="Name"
+                      className="form-control my-3 p-4"
+                    />
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="col-lg-7">
+                    <textarea
+                      type="bio"
+                      placeholder="Bio"
+                      className="form-control my-3 p-4"
+                    />
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="col-lg-7">
+                    <button type="button" className="btn1">
+                      Create Account
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </body>
+  );
+}
+
+export default index;
